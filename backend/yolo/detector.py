1d87744b71f4f18eaf0beb274e1879b66fc952e5
Max-JI64 <swoo646464@gmail.com>
2025-12-05 15:43:33 +0900

    only mouse detect

--- backend/yolo/detector.py
+++ backend/yolo/detector.py
@@ -37,9 +37,10 @@ class ObjectDetector:
             for box in r.boxes: #탐지된 각 객체들에 대하여 
                 cls_id = int(box.cls[0].item()) #클래스 아이디 추출
                 class_name = self.names.get(cls_id, str(cls_id)) #클래스 딕셔너리에서 해당 아이디의 이름을 가져온다, 없으면 그냥 클래스 아이디 반환
-                x1, y1, x2, y2 = map(int, box.xyxy[0].tolist()) #물체의 좌상단, 우하단 위치 좌표를 정수로 가져온다
 
                 if class_name == 'mouse': #마우스가 탐지된 경우
+                    x1, y1, x2, y2 = map(int, box.xyxy[0].tolist()) #물체의 좌상단, 우하단 위치 좌표를 정수로 가져온다
+                    
                     mouse_detected = True #탐지 플래그 세우기
 
                     # 마우스 박스의 중심좌표 계산
@@ -49,13 +50,10 @@ class ObjectDetector:
 
                     color = (0, 0, 255) # Red
                     label = f"WARNING: {class_name} detected!" #라벨에 탐지 경고 메세지 출력
-                else:
-                    color = (0, 255, 0) # Green
-                    label = class_name #라벨에 클래스 이름 출력
 
-                cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2) #탐지된 객체의 좌표와 색깔로 네모 박스를 두께 2로 그림
-                cv2.putText(frame, label, (x1, y1 - 10),#라벨은 좌상단 좌표 바로 위에 출력
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.6, color, 2) #라벨을 글씨로 작성
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2) #탐지된 객체의 좌표와 색깔로 네모 박스를 두께 2로 그림
+                    cv2.putText(frame, label, (x1, y1 - 10),#라벨은 좌상단 좌표 바로 위에 출력
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.6, color, 2) #라벨을 글씨로 작성
 
         if mouse_detected: #마우스가 탐지된 경우
             cv2.putText(frame, "WARNING: MOUSE DETECTED!", (50, 100), #스트리밍 화면 좌상단에 경고문구 출력
